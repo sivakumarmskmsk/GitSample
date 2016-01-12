1da80509fd7c57a17d18922187d3b31de7df7554
sivakumarmskmsk <sivakumar.msk.msk@gmail.com>
2016-01-12 16:35:36 +0530

    Git sample
    git desc

---
+++ LoggingClass.java
@@ -0,0 +1,23 @@
+package com.example.logging;
+
+import org.testng.annotations.Test;
+
+public class LoggingClass {
+
+	@Test(priority = 0)
+	public void methodAddingNumbers() {
+		System.out.println("Helloo.. Im in method adding numbers");
+	}
+
+	@Test(priority = 1)
+	public void dividedByZero() {
+		System.out.println("Helloo.. Im in method divided by zero");
+		int e = 1 / 0;
+	}
+
+	@Test(dependsOnMethods = { "dividedByZero" })
+	public void methodSkip() {
+		System.out.println("Helloo.. Im in method skip");
+	}
+
+}
